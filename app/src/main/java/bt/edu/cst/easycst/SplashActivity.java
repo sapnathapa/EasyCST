343e3bb6e65b5e625ba4ac62440b460c1d993052
nathtek136 <nathtek136@gmail.com>
2018-11-01 15:19:45 +0600

    minimal chnges

--- app/src/main/java/bt/edu/cst/easycst/SplashActivity.java
+++ app/src/main/java/bt/edu/cst/easycst/SplashActivity.java
@@ -1,14 +1,19 @@
 package bt.edu.cst.easycst;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, Home.class));
+        // close splash activity
+        finish();
     }
 }
