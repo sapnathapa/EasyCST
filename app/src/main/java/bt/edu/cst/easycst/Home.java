f90d21c945dae625f3a9c38e1feb718cca165cd8
nathtek136 <nathtek136@gmail.com>
2018-11-01 15:33:25 +0600

    doks push rwquest

--- app/src/main/java/bt/edu/cst/easycst/Home.java
+++ app/src/main/java/bt/edu/cst/easycst/Home.java
@@ -2,6 +2,7 @@ package bt.edu.cst.easycst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Home extends AppCompatActivity {
 
