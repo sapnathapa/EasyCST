69ba08d1838ff35279cd1af3f926ad8294655a3f
nathtek136 <nathtek136@gmail.com>
2018-12-26 11:41:05 +0600

    done with reg page backend also

--- app/src/main/java/bt/edu/cst/easycst/maintain.java
+++ app/src/main/java/bt/edu/cst/easycst/maintain.java
@@ -1,15 +1,169 @@
 package bt.edu.cst.easycst;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class maintain extends AppCompatActivity {
+    // Creating EditText.
+    EditText room, phone, descript;
+
+    // Creating button;
+    Button Register;
+
+    // Creating Volley RequestQueue.
+    RequestQueue requestQueue;
+
+    // Create string variable to hold the EditText Value.
+    String RoomHolder, PhoneHolder, DescriptHolder;
+
+    // Creating Progress dialog.
+    ProgressDialog progressDialog;
+
+    // Storing server url into String variable.
+    String HttpUrl = "http://192.168.42.249/easycst/maintain.php";
+
+    Boolean CheckEditText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintain);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Assigning ID's to EditText.
+        room = (EditText) findViewById(R.id.room);
+
+        phone = (EditText) findViewById(R.id.phone);
+
+        descript = (EditText) findViewById(R.id.descript);
+
+
+        // Assigning ID's to Button.
+        Register = (Button) findViewById(R.id.submit);
+
+
+
+
+        // Creating Volley newRequestQueue .
+        requestQueue = Volley.newRequestQueue(maintain.this);
+
+        // Assigning Activity this to progress dialog.
+        progressDialog = new ProgressDialog(maintain.this);
+
+        // Adding click listener to button.
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckEditTextIsEmptyOrNot();
+                if(CheckEditText){
+                    MRegistration();
+                }
+                else {
+                    Snackbar.make(view, "Please fill all fields ", Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void MRegistration(){
+
+        // Showing progress dialog at user registration time.
+        progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
+        progressDialog.show();
+
+        // Creating string request with post method.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing Echo Response Message Coming From Server.
+
+                        Toast.makeText(maintain.this, ServerResponse, Toast.LENGTH_LONG).show();
+                        if(ServerResponse.equals("Registration Successfull")){
+                            startActivity(new Intent(maintain.this, Home.class));
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing error message if something goes wrong.
+                        Toast.makeText(maintain.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // Adding All values to Params.
+                // The firs argument should be same sa your MySQL database table columns.
+                params.put("room", RoomHolder);
+                params.put("phone", PhoneHolder);
+                params.put("descript", DescriptHolder);
+                return params;
+            }
+
+        };
+
+        // Creating RequestQueue.
+        RequestQueue requestQueue = Volley.newRequestQueue(maintain.this);
+
+        // Adding the StringRequest object into requestQueue.
+        requestQueue.add(stringRequest);
+
+    }
+
+
+    public void CheckEditTextIsEmptyOrNot(){
+
+        // Getting values from EditText.
+        RoomHolder = room.getText().toString().trim();
+        PhoneHolder= phone.getText().toString().trim();
+        DescriptHolder = descript.getText().toString().trim();
+
+
+        // Checking whether EditText value is empty or not.
+        if(TextUtils.isEmpty(RoomHolder) || TextUtils.isEmpty(RoomHolder) || TextUtils.isEmpty(DescriptHolder))
+        {
+
+            // If any of EditText is empty then set variable value as False.
+            CheckEditText = false;
+
+        }
+        else {
+
+            // If any of EditText is filled then set variable value as True.
+            CheckEditText = true ;
+        }
     }
 }
--- app/src/main/java/bt/edu/cst/easycst/register.java
+++ app/src/main/java/bt/edu/cst/easycst/register.java
@@ -1,33 +1,87 @@
 package bt.edu.cst.easycst;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
+    // Creating EditText.
+    EditText name, uid, email, password ;
+
+    // Creating button;
+    Button Register;
+
+    //Creating Spinner
+    Spinner spinners;
+
+    // Creating Volley RequestQueue.
+    RequestQueue requestQueue;
+
+    // Create string variable to hold the EditText Value.
+    String NameHolder, EmailHolder, PasswordHolder, DepartmentHolder, GenderHolder, UidHolder;
+
+    // Creating Progress dialog.
+    ProgressDialog progressDialog;
+
+    //Creating RadioGroup
+    RadioGroup gender;
+
+    // Storing server url into String variable.
+    String HttpUrl = "http://192.168.42.249/easycst/register.php";
+
+    Boolean CheckEditText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Assigning ID's to EditText.
+        name = (EditText) findViewById(R.id.name);
+
+        email = (EditText) findViewById(R.id.regemail);
+
+        password = (EditText) findViewById(R.id.pass1);
+
+        uid = (EditText) findViewById(R.id.uid);
+
+        gender = (RadioGroup) findViewById(R.id.gender);
+
+        spinners=(Spinner) findViewById(R.id.spinner);
+
+        // Assigning ID's to Button.
+        Register = (Button) findViewById(R.id.regButton);
+
+        //Creating Spinner
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        Button register = (Button) findViewById(R.id.regButton);
-
         List<String> departments = new ArrayList<String>();
-        departments.add("Please Select Your Department");
-        departments.add("Civil Engg. and Architecture");
-        departments.add("Electrical");
-        departments.add("Electronics and Communication");
-        departments.add("Information Technology");
-
+        departments.add("Civil Engg. and Architecture Department");
+        departments.add("Electrical Engg Department");
+        departments.add("Electronics and Communication Department");
+        departments.add("Information Technology Department");
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,departments);
         spinner.setPrompt("Department");
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -35,15 +89,137 @@ public class register extends AppCompatActivity {
         //department.setPrompt("Department");
         spinner.setAdapter(dataAdapter);
 
-        register.setOnClickListener(new View.OnClickListener() {
+
+        GenderHolder = (String) ((RadioButton) this.gender.findViewById(this.gender.getCheckedRadioButtonId())).getText();
+
+        // Creating Volley newRequestQueue .
+        requestQueue = Volley.newRequestQueue(register.this);
+
+        // Assigning Activity this to progress dialog.
+        progressDialog = new ProgressDialog(register.this);
+
+        // Adding click listener to button.
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(register.this,Home.class);
-                finish();
-                startActivity(intent);
+                DepartmentHolder = String.valueOf(spinners.getSelectedItem());
+                CheckEditTextIsEmptyOrNot();
+                if(CheckEditText){
+                    UserRegistration();
+                }
+                else {
+                    Snackbar.make(view, "Please fill all fields", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                }
+
             }
         });
+    }
 
+    public void UserRegistration(){
+
+        // Showing progress dialog at user registration time.
+        progressDialog.setMessage("Please Wait, We are Inserting Your Data on Server");
+        progressDialog.show();
+
+        // Creating string request with post method.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String ServerResponse) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing Echo Response Message Coming From Server.
+
+                        Toast.makeText(register.this, ServerResponse, Toast.LENGTH_LONG).show();
+                        if(ServerResponse.equals("Registration Successfull")){
+                            startActivity(new Intent(register.this, Home.class));
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                        // Hiding the progress dialog after all task complete.
+                        progressDialog.dismiss();
+
+                        // Showing error message if something goes wrong.
+                        Toast.makeText(register.this, volleyError.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                // Creating Map String Params.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // Adding All values to Params.
+                // The firs argument should be same sa your MySQL database table columns.
+                params.put("name", NameHolder);
+                params.put("uid", UidHolder);
+                params.put("password", PasswordHolder);
+                params.put("gender", GenderHolder);
+                params.put("email", EmailHolder);
+                params.put("department", DepartmentHolder);
+
+                return params;
+            }
+
+        };
+
+        // Creating RequestQueue.
+        RequestQueue requestQueue = Volley.newRequestQueue(register.this);
+
+        // Adding the StringRequest object into requestQueue.
+        requestQueue.add(stringRequest);
 
     }
+
+
+    public void CheckEditTextIsEmptyOrNot(){
+
+        // Getting values from EditText.
+        NameHolder = name.getText().toString().trim();
+        UidHolder= uid.getText().toString().trim();
+        PasswordHolder = password.getText().toString().trim();
+        EmailHolder = email.getText().toString().trim();
+
+
+        // Checking whether EditText value is empty or not.
+        if(TextUtils.isEmpty(NameHolder) || TextUtils.isEmpty(EmailHolder) || TextUtils.isEmpty(PasswordHolder)|| TextUtils.isEmpty(UidHolder))
+        {
+
+            // If any of EditText is empty then set variable value as False.
+            CheckEditText = false;
+
+        }
+        else {
+
+            // If any of EditText is filled then set variable value as True.
+            CheckEditText = true ;
+            if(isEmail(EmailHolder)){
+                CheckEditText = true;
+            }
+            else{
+                CheckEditText = false;
+                EditText myEditText = (EditText)findViewById(R.id.regemail);
+                //myEditText.setTextColor(Color.RED);
+                myEditText.setText("");
+                myEditText.setHintTextColor(Color.RED);
+                myEditText.setHint("Please enter correct email");
+                myEditText.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+
+            }
+        }
+    }
+    public final static boolean isEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
 }
