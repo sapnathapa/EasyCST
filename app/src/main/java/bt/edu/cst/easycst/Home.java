3ddc751520a390e0650204d0a289b6b10aa3a392
nathtek136 <nathtek136@gmail.com>
2019-02-18 22:14:43 +0600

    rate and share done

--- app/src/main/java/bt/edu/cst/easycst/Home.java
+++ app/src/main/java/bt/edu/cst/easycst/Home.java
@@ -1,11 +1,14 @@
 package bt.edu.cst.easycst;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -16,9 +19,11 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         ImageView maintain = (ImageView)findViewById(R.id.maintain);
-        ImageView rate = (ImageView) findViewById(R.id.rate);
+        ImageView about = (ImageView) findViewById(R.id.about);
         ImageView feedback =(ImageView) findViewById(R.id.feedback);
         ImageView attendance =(ImageView) findViewById(R.id.attendance);
+        ImageView rate =(ImageView) findViewById(R.id.rate);
+        ImageView share =(ImageView) findViewById(R.id.share);
 
         maintain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +53,36 @@ public class Home extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Home.this,Attendance.class);
                 startActivity(intent);
+            }
+        });
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=PackageName")));
+            }
+        });
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this,About.class);
+                startActivity(intent);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Easy CST");
+                    String shareMessage= "\nLet me recommend you this application\n\n";
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "choose one"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
             }
         });
 
