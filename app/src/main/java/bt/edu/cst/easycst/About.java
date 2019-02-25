6b24cb7482a83f03a3c667ea2ebeb17d9d2eef6d
karmakartse <kartse100@gmail.com>
2019-02-25 21:10:21 +0600

    edit about

--- app/src/main/java/bt/edu/cst/easycst/About.java
+++ app/src/main/java/bt/edu/cst/easycst/About.java
@@ -22,13 +22,13 @@ public class About extends AppCompatActivity {
         String myData = "<html><body style=\"text-align:justify\"><h2 align=\"center\">Version : 1.0</h2><h2 align=\"center\">About App</h2>EasyCST, a platform where the student can have easy access to the service like maintenance.\n" +
                 "        Has privilege to track ones attendance and own editable timetable.\n" +
                 "        User can provide feedback and suggestion for the betterment of the college.\n" +
-                "<h2 align=\"center\">Developers</h2> 1. Mr.Tek Nath Acharya(nathtek136@gmail.com)<br>" +
-                "        2. Mr.Karma Dori(karmadorgey@gmail.com)<br>" +
-                "        3. Ms.Deepika Suberi(deepikasbr@gmail.com)<br>" +
-                "        4. Mr.Karma Tshewang(kartse100@gmail.com).\n" +
-                "<h2 align=\"center\">Special Thanks</h2> 1. Mr.Parshuram Dhungyel(parshuram.cst@rub.<br>edu.bt)<br>" +
-                "        2. Mr.Hari Kafley(hari.kafley@itechnologies.bt)<br>" +
-                "        3. Mr.Khusant Chhetri(0215518.cst@rub.edu.bt)</body></html>";
+                "<h2 align=\"center\">Developers</h2> 1.<a href =\"mail to : nathtek136@gmail.com\">Mr.Tek Nath Acharya</a><br>" +
+                "         2.<a href =\"mail to : karmadorgey@gmail.com\">Mr.Karma Dorji</a><br>" +
+                "        3.<a href =\"mail to : deepikasbr@gmail.com\">Ms.Deepika Suberi</a><br>" +
+                "        4.<a href =\"mail to : kartse100@gmail.com\"> Mr.Karma Tshewang</a>\n" +
+                "<h2 align=\"center\">Special Thanks</h2> 1.<a href =\"mail to : parshuram.cst@rub.edu.bt\">Mr.Parshuram Dhungyel</a><br>" +
+                "        2.<a href =\"mail to : hari.kafley@itechnologies.bt\">Mr.Hari Kafley</a><br>" +
+                "        3.<a href =\"mail to : 0215518.cst@rub.edu.bt\">Mr.Khusant Chhetri</a></body></html>";
 
 //        String myData = "Hello World! This tutorial is to show demo of displaying text with justify alignment in WebView.";
 
