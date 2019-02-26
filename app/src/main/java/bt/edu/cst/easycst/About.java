25eff912b807700d58bdb72ba855f2f05e243064
karmakartse <kartse100@gmail.com>
2019-02-26 13:38:51 +0600

    karma

--- app/src/main/java/bt/edu/cst/easycst/About.java
+++ app/src/main/java/bt/edu/cst/easycst/About.java
@@ -2,8 +2,12 @@ package bt.edu.cst.easycst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class About extends AppCompatActivity {
     WebView webView;
@@ -17,20 +21,21 @@ public class About extends AppCompatActivity {
         webView= findViewById(R.id.webView);
         webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        TextView tv = (TextView)findViewById(R.id.text);
+        //tv.setMovementMethod(LinkMovementMethod.getInstance());
+//      webView.setData(LinkMovementMethod.getInstance());
         String htmlText = " %s ";
 
-        String myData = "<html><body style=\"text-align:justify\"><h2 align=\"center\">Version : 1.0</h2><h2 align=\"center\">About App</h2>EasyCST, a platform where the student can have easy access to the service like maintenance.\n" +
-                "        Has privilege to track ones attendance and own editable timetable.\n" +
-                "        User can provide feedback and suggestion for the betterment of the college.\n" +
-                "<h2 align=\"center\">Developers</h2> 1.<a href =\"mail to : nathtek136@gmail.com\">Mr.Tek Nath Acharya</a><br>" +
-                "         2.<a href =\"mail to : karmadorgey@gmail.com\">Mr.Karma Dorji</a><br>" +
-                "        3.<a href =\"mail to : deepikasbr@gmail.com\">Ms.Deepika Suberi</a><br>" +
-                "        4.<a href =\"mail to : kartse100@gmail.com\"> Mr.Karma Tshewang</a>\n" +
-                "<h2 align=\"center\">Special Thanks</h2> 1.<a href =\"mail to : parshuram.cst@rub.edu.bt\">Mr.Parshuram Dhungyel</a><br>" +
-                "        2.<a href =\"mail to : hari.kafley@itechnologies.bt\">Mr.Hari Kafley</a><br>" +
-                "        3.<a href =\"mail to : 0215518.cst@rub.edu.bt\">Mr.Khusant Chhetri</a></body></html>";
+        String myData = "<html><body style=\"text-align:justify\"><h2 align=\"center\">Version : 1.0</h2><h2 align=\"center\">About App</h2>EasyCST is a platform for the student to have easy access to various services such as " +
+                "registration for maintenance services,self-tracking,adding and editing timetable and providing feedback for college" +
+                "<h2 align=\"center\">Developers</h2> 1.<a href =\"mailto : nathtek136@gmail.com\" style = \"text-decoration:none\">Mr.Tek Nath Acharya</a><br>" +
+                "         2.<a href =\"mailto : karmadorgey@gmail.com\" style = \"text-decoration:none\">Mr.Karma Dorji</a><br>" +
+                "        3.<a href =\"mailto : deepikasbr@gmail.com\" style = \"text-decoration:none\">Ms.Deepika Suberi</a><br>" +
+                "        4.<a href =\"mailto : kartse100@gmail.com\" style = \"text-decoration:none\"> Mr.Karma Tshewang</a>\n" +
+                "<h2 align=\"center\">Special Thanks</h2> 1.<a href =\"mailto : parshuram.cst@rub.edu.bt\" style = \"text-decoration:none\">Mr.Parshuram Dhungyel</a><br>" +
+                "        2.<a href =\"mailto : hari.kafley@itechnologies.bt\" style = \"text-decoration:none\">Mr.Hari Kafley</a><br>" +
+                "        3.<a href =\"mailto : 0215518.cst@rub.edu.bt\" style = \"text-decoration:none\">Mr.Khusant Chhetri</a></body></html>";
 
-//        String myData = "Hello World! This tutorial is to show demo of displaying text with justify alignment in WebView.";
 
         webView.loadData(String.format(htmlText,myData),"text/html","utf-8");
     }
