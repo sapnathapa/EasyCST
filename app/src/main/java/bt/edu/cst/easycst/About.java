659ea4bb5bf00263f73d7366c8b50b37438cb959
karmakartse <kartse100@gmail.com>
2019-02-28 23:03:29 +0600

    check updated on about page 2

--- app/src/main/java/bt/edu/cst/easycst/About.java
+++ app/src/main/java/bt/edu/cst/easycst/About.java
@@ -19,7 +19,7 @@ import org.w3c.dom.Text;
 //import com.google.android.gms.common.api.GoogleApiClient;
 
 public class About extends AppCompatActivity {
-//    WebView webView;
+
     WebSettings webSettings;
 //    private GoogleApiClient client;
     private WebView view;
@@ -57,14 +57,15 @@ public class About extends AppCompatActivity {
 
         String htmlText = " %s ";
 
-        String myData = "<html><body style=\"text-align:justify\"><h2 align=\"center\">Version : 1.0</h2><h2 align=\"center\">About App</h2>EasyCST is a platform for the student to have easy access to various services such as " +
-                "registration for maintenance services,self-tracking,adding and editing timetable and providing feedback for college" +
-                "<h2 align=\"center\">Developers</h2> 1.<a href =\"mailto: nathtek136@gmail.com\" style = \"text-decoration:none\">Mr.Tek Nath Acharya</a><br>" +
-                "         2.<a href =\"mailto: karmadorgey@gmail.com\" style = \"text-decoration:none\">Mr.Karma Dorji</a><br>" +
-                "        3.<a href =\"mailto: deepikasbr@gmail.com\" style = \"text-decoration:none\">Ms.Deepika Suberi</a><br>" +
-                "        4.<a href =\"mailto: kartse100@gmail.com\" style = \"text-decoration:none\"> Mr.Karma Tshewang</a>\n" +
-                "<h2 align=\"center\">Special Thanks</h2> 1.<a href =\"mailto: parshuram.cst@rub.edu.bt\" style = \"text-decoration:none\">Mr.Parshuram Dhungyel</a><br>" +
-                "        2.<a href =\"mailto: hari.kafley@itechnologies.bt\" style = \"text-decoration:none\">Mr.Hari Kafley</a><br>" +
+        String myData = "<html><body style=\"text-align:justify\"><h2 align=\"center\">" +
+                "Version : 1.0</h2>" +
+                "<h2 align=\"center\">About App</h2>EasyCST is a platform for the student to have easy access to various services such as registration for maintenance services, self-tracking, adding and editing timetable and providing feedback for college." +
+                "<h2 align=\"center\">Developers</h2>1.<a href =\"mailto: nathtek136@gmail.com\" style = \"text-decoration:none\">Mr.Tek Nath Acharya</a><br><br>" +
+                "         2.<a href =\"mailto: karmadorgey@gmail.com\" style = \"text-decoration:none\">Mr.Karma Dorji</a><br><br>" +
+                "        3.<a href =\"mailto: deepikasbr@gmail.com\" style = \"text-decoration:none\">Ms.Deepika Suberi</a><br><br>" +
+                "        4.<a href =\"mailto: kartse100@gmail.com\" style = \"text-decoration:none\"> Mr.Karma Tshewang</a>" +
+                "<h2 align=\"center\">Special Thanks</h2>1.<a href =\"mailto: parshuram.cst@rub.edu.bt\" style = \"text-decoration:none\">Mr.Parshuram Dhungyel(project guide)</a><br><br>" +
+                "        2.<a href =\"mailto: hari.kafley@itechnologies.bt\" style = \"text-decoration:none\">Mr.Hari Kafley</a><br><br>" +
                 "        3.<a href =\"mailto: 0215518.cst@rub.edu.bt\" style = \"text-decoration:none\">Mr.Khusant Chhetri</a></body></html>";
 
 
--- app/src/main/java/bt/edu/cst/easycst/Help.java
+++ app/src/main/java/bt/edu/cst/easycst/Help.java
@@ -2,8 +2,12 @@ package bt.edu.cst.easycst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class Help extends AppCompatActivity {
 
@@ -12,6 +16,30 @@ public class Help extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+//        String[] list = {"About Company", "Home Page", "Contact"};
+//
+//        Adapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+//
+//        ListView listView = (ListView) findViewById(R.id.listId);
+//
+//        listView.setAdapter((ListAdapter) adapter);
+//
+//        listView.setOnItemClickListener(
+//
+//                new AdapterView.OnItemClickListener() {
+//
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        String listPosition = String.valueOf(parent.getItemAtPosition(position));
+//                        /*after clicking the first row of the list show the following info :
+//                        NAME: Some name
+//                        ADDRESS: Somewhere
+//                         */
+//                    }
+//                }
+//        );
+
 
     }
 }
